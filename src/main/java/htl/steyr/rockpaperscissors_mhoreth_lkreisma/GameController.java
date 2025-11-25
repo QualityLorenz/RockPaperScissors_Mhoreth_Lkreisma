bd2b2c4874e1e66bd72caf6c23275a1eab135e9d
Manuel Horeth <mhoreth@htl-steyr.ac.at>
2025-11-25 10:21:04 +0100

    created basic fxml. changed names to GameApplication.java and GameController.java

--- src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/GameController.java
+++ src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/GameController.java
@@ -1,14 +1,24 @@
 package htl.steyr.rockpaperscissors_mhoreth_lkreisma;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class GameController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void rockButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void paperButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void scissorsButtonClicked(ActionEvent actionEvent) {
     }
 }
