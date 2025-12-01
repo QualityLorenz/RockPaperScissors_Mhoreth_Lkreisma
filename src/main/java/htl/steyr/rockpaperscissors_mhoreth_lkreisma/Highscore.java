296a9cca8b5fbb26dc350030876705e27d183cce
kreis <lkreisma@htl-steyr.ac.at>
2025-12-02 00:16:13 +0100

    created new branch highscore_feature and made getter and setter methods

---
+++ src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/Highscore.java
@@ -0,0 +1,33 @@
+package htl.steyr.rockpaperscissors_mhoreth_lkreisma;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Arrays;
+
+public class Highscore {
+
+    private int highscore;
+    private int currentScore;
+
+
+    public int getHighscore() {
+        return highscore;
+    }
+
+
+    public void setHighscore(int highscore) {
+
+    }
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+
+
+}
