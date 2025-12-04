d5eb2de7384a72d468ce829961c458ec65b3424f
kreis <lkreisma@htl-steyr.ac.at>
2025-12-04 12:09:41 +0100

    highscore function finished can now be used and styled

--- src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/GameController.java
+++ src/main/java/htl/steyr/rockpaperscissors_mhoreth_lkreisma/GameController.java
@@ -9,7 +9,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-import java.io.IOException;
 import java.util.Objects;
 import java.util.Random;
 
